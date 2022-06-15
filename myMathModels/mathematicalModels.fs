1007f34979b142c9fe9002a0276b7f35ef7c47e8
Joy Akinyi <Joy.recho7@gmail.com>
2022-06-15 14:04:35 +0300

    Add project files.

---
+++ myMathModels/mathematicalModels.fs
@@ -0,0 +1,17 @@
+﻿namespace myMathModels
+open System
+
+type myCalculations(input1 : decimal, input2 : decimal) = 
+      member this.checks =       
+          if input1 = 0m then 
+              raise(ArgumentException("Please input values"))
+          else input1
+
+      member this.Subtraction  =
+             input1 - input2
+
+      member this.Addition = 
+            input1 + input2
+
+      member this.Multiplication (input3 :decimal) =
+             (input1 * input2)/input3
